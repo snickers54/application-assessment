6db4374b93e1948618de3dd50d9fd67792d50ed6
julien singler <julien.singler@gmail.com>
2016-02-09 01:57:20 +0100

    first commit

---
+++ yieldify/index.js
@@ -0,0 +1,16 @@
+var core;
+window.onload = function(){
+    core = new Core({type:"Canvas", colors:['black', 'blue', 'orange', 'brown', 'red', 'green', 'grey', 'whitesmoke', 'pink', 'darkblue', 'purple']});
+
+    document.onclick = function(event) {
+        // It seems, this code is to fix < IE9 even system ..
+        if (event === undefined) event = window.event;
+        var target = 'target' in event? event.target : event.srcElement;
+        if (target.getAttribute("id") !== "pause_resume"
+            && target.getAttribute("name") !== "nb_particles" &&
+            target.tagName.toLowerCase() !== "button") {
+            core.add(event.pageX, event.pageY);
+        }
+    };
+
+}
